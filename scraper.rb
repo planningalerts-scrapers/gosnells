d41db320d4a351e9ed312618f4f319a227c6cf0d
Matthew Landauer <mlandauer@gmail.com>
2019-06-09 17:48:18 +1000

    Use original form of address

--- scraper.rb
+++ scraper.rb
@@ -4,6 +4,5 @@ IconScraper.scrape_with_params(
   url: "http://apps.gosnells.wa.gov.au/ICON",
   period: "last14days"
 ) do |record|
-  record["address"] = record["address"].gsub(",", "")
   IconScraper.save(record)
 end
